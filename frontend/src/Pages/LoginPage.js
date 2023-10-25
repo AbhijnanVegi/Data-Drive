2bb8c6e4af8ae52d57487e7410edd6f2cc61fb6c
rudransh-singh <rudransh.s@research.iiit.ac.in>
2023-10-25 21:56:29 +0530

    added background login page

--- frontend/src/Pages/LoginPage.js
+++ frontend/src/Pages/LoginPage.js
@@ -1,13 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import { Tabs, Input, Button, Form, ConfigProvider } from 'antd';
 import '../css/LoginPage.css'
 import axios from 'axios';
+import Particles from "react-particles";
+import { loadSlim } from "tsparticles-slim"; // if you are going to use `loadSlim`, install the "tsparticles-slim" package too.
 
 const { TabPane } = Tabs;
 
 const LoginPage = () => {
     const [activeTab, setActiveTab] = useState('login');
 
+    const particlesInit = useCallback(async engine => {
+        console.log(engine);
+        // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+        // starting from v2 you can add only the features you need reducing the bundle size
+        //await loadFull(engine);
+        await loadSlim(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(async container => {
+        await console.log(container);
+    }, []);
     const handleTabChange = (tab) => {
         setActiveTab(tab);
     };
@@ -21,7 +35,7 @@ const LoginPage = () => {
             .then((response) => {
                 console.log("response")
                 console.log(response)
-                if(response.status === 200)
+                if (response.status === 200)
                     window.location.href = '/dashboard';
                 else
                     alert(response.data.message)
@@ -67,13 +81,90 @@ const LoginPage = () => {
                 },
             }}
         >
+            <Particles
+                id="tsparticles"
+                className="particles-canvas"
+                init={particlesInit}
+                loaded={particlesLoaded}
+                options={{
+                    background: {
+                        color: {
+                            value: "#ffffff",
+                        },
+                    },
+                    fpsLimit: 120,
+                    interactivity: {
+                        events: {
+                            onClick: {
+                                enable: true,
+                                mode: "push",
+                            },
+                            onHover: {
+                                enable: true,
+                                mode: "repulse",
+                            },
+                            resize: true,
+                        },
+                        modes: {
+                            push: {
+                                quantity: 4,
+                            },
+                            repulse: {
+                                distance: 200,
+                                duration: 0.4,
+                            },
+                        },
+                    },
+                    particles: {
+                        color: {
+                            value: "#000000",
+                        },
+                        links: {
+                            color: "#000000",
+                            distance: 150,
+                            enable: true,
+                            opacity: 0.5,
+                            width: 1,
+                        },
+                        move: {
+                            direction: "none",
+                            enable: true,
+                            outModes: {
+                                default: "bounce",
+                            },
+                            random: false,
+                            speed: 6,
+                            straight: false,
+                        },
+                        number: {
+                            density: {
+                                enable: true,
+                                area: 800,
+                            },
+                            value: 80,
+                        },
+                        opacity: {
+                            value: 0.5,
+                        },
+                        shape: {
+                            type: "circle",
+                        },
+                        size: {
+                            value: { min: 1, max: 5 },
+                        },
+                    },
+                    detectRetina: true,
+                }}
+            />
             <div className='login-container'>
+
                 {/* create text at top of login box */}
                 <div className='login-text'>
                     <h1>Welcome to Data Drive!</h1>
                     <p>Sign in or create an account to get started.</p>
                 </div>
                 <div className='login-box'>
+
                     <Tabs activeKey={activeTab} onChange={handleTabChange}>
                         <TabPane tab="Login" key="login">
                             <Form name="login-form" onFinish={handleLoginSubmit}>
