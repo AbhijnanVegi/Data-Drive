a81ddc57896543e2434c507d4e183ca95d8cdfb6
rudransh-singh <rudransh.s@research.iiit.ac.in>
2023-10-26 22:19:28 +0530

    set up chonky on HomePage

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { setChonkyDefaults } from 'chonky';
+import { ChonkyIconFA } from 'chonky-icon-fontawesome';
+import { FullFileBrowser } from 'chonky';
+import '../css/HomePage.css'
+
+setChonkyDefaults({ iconComponent: ChonkyIconFA });
 
 const HomePage = () => {
+    const files = [
+        { id: 'lht', name: 'Projects', isDir: true },
+        {
+            id: 'mcd',
+            name: 'chonky-sphere-v2.png',
+            thumbnailUrl: 'https://chonky.io/chonky-sphere-v2.png',
+        },
+    ];
+    const folderChain = [{ id: 'xcv', name: 'Demo', isDir: true }];
     return (
-        <div className="home">
-            <h1>Home Page</h1>
+        <div className='full-page'>
+            <div className='chonky'>
+                <FullFileBrowser files={files} folderChain={folderChain} />
+            </div>
         </div>
     );
 }
